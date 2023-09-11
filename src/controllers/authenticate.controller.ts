3f77c33336f98fc4f1ad0a473d65e325240583c6
Lucas Silva <lucassilva080397@gmail.com>
2023-09-11 17:05:35 -0300

    .

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const authBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthBodySchema = z.infer<typeof authBodySchema>
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(authBodySchema))
+  async handle(@Body() body: AuthBodySchema) {
+    const { email, password } = body
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('User credentails do not match.')
+    }
+
+    const isPasswordValid = await compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentails do not match.')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return {
+      access_token: accessToken,
+    }
+  }
+}
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchame = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchame)
+type PageQueryParamSchame = z.infer<typeof pageQueryParamSchame>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchame) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return { questions }
+  }
+}
