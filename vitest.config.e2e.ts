7f5f36942eba128241db0707e190bb1e1a327f6d
Lucas Silva <lucassilva080397@gmail.com>
2023-09-12 08:53:13 -0300

    .

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,5 +1,5 @@
 import swc from 'unplugin-swc'
-import { configDefaults, defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
-    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+    setupFiles:['./test/setup-e2e.ts'],
   },
   plugins: [
     tsConfigPaths(),
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
 import swc from 'unplugin-swc'
-import { configDefaults, defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
     globals: true,
     root: './',
-    exclude: [...configDefaults.exclude, '**/data/pg/**'],
   },
   plugins: [
     tsConfigPaths(),
